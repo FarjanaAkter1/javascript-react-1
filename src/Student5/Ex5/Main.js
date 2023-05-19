e128f8a895ebfa181eb8d227b135454f59dc2c3c
Farjana Akter <fakter@xpanxion.com>
2023-05-19 15:18:38 -0400

    update my example5

--- src/Student5/Ex5/Main.js
+++ src/Student5/Ex5/Main.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import './Main.css'
+import React from "react";
+
+import "./Main.css";
+
+import { useState } from "react";
 
 export const Main = () => {
+  const [showAccountNumber, setShowAccountNumber] = useState(false);
+  const toggleAccountNumber = () => {
+    setShowAccountNumber(!showAccountNumber);
+  };
   return (
     <div>
-        Hello I'm student id: 1.
+      {" "}
+      <table>
+        {" "}
+        <thead>
+          {" "}
+          <th className="th">
+            <td>Account Information:</td>{" "}
+          </th>{" "}
+        </thead>{" "}
+        <tbody>
+          {" "}
+          <tr>
+            <td>Balance: </td> <td> $1,000,000</td>{" "}
+          </tr>{" "}
+          <tr>
+            <td>Interest: </td> <td> 3.0% </td>
+          </tr>{" "}
+          {showAccountNumber && (
+            <tr>
+              {" "}
+              <td>Account Number:</td> <td>11-1234</td>{" "}
+            </tr>
+          )}{" "}
+        </tbody>{" "}
+      </table>{" "}
+      <p>
+        {" "}
+        <button onClick={toggleAccountNumber}>
+          {showAccountNumber ? "Hide" : "Show"} Account{" "}
+        </button>{" "}
+      </p>{" "}
     </div>
-  )
-}
+  );
+};
+
+export default Main;
